5df28b884e5796531590b13d7761d711b08d4bc9
William G (VinuSion) <revisionthegame@gmail.com>
2023-09-04 18:36:20 -0500

    reverted stuff

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,7 +5,6 @@ import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-import { BrowserView, MobileView } from "react-device-detect";
 
 const ProjectCard = ({
   index,
@@ -18,69 +17,14 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <BrowserView>
-        <Tilt
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-        >
-          <div className="relative w-full h-[230px]">
-            <img
-              src={image}
-              alt={name}
-              className="w-full h-full object-cover rounded-2xl"
-            />
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-              <div
-                onClick={() => window.open(demo, "__blank")}
-                className="bg-gradient-to-r from-[#0d3c36] to-[#051614] w-10 h-10 rounded-full flex justify-center items-center cursor-pointer mr-3"
-              >
-                <svg
-                  className="w-1/3 h-1/3 object-contain"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="0.875em"
-                  viewBox="0 0 512 512"
-                  fill="#ffffff"
-                >
-                  <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
-                </svg>
-              </div>
-
-              <div
-                onClick={() => window.open(source_code_link, "__blank")}
-                className="bg-gradient-to-r from-[#0d3c36] to-[#051614] w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  src={github}
-                  alt={github}
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-5">
-            <h3 className="text-white font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px]">{description}</p>
-          </div>
-
-          <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <p
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] ${tag.color}`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
-        </Tilt>
-      </BrowserView>
-
-      <MobileView>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
         <div className="relative w-full h-[230px]">
           <img
             src={image}
@@ -121,7 +65,7 @@ const ProjectCard = ({
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -131,7 +75,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </MobileView>
+      </Tilt>
     </motion.div>
   );
 };
